b9243a3d7323544cfd2a33c41fe164598bb501b8
valentinaz0306 <valzapataz@gmail.com>
2020-08-27 22:13:17 -0500

    Actualizacion final

--- app/src/main/java/co/intro/myapplication/MainActivity.java
+++ app/src/main/java/co/intro/myapplication/MainActivity.java
@@ -8,7 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+//import android.widget.Toast;
+
 
 import java.util.ArrayList;
 
